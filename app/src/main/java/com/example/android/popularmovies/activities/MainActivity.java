14d44b08b7d757586b553a3e27e6396370f5ebad
DavidPrecopia <35779726+DavidPrecopia@users.noreply.github.com>
2018-05-17 15:26:11 -0400

    MainActivity's layout working as expected.
    Simply need to add the below to fix SwipeRefreshLayout
    app:layout_behavior="@string/appbar_scrolling_view_behavior"

--- app/src/main/java/com/example/android/popularmovies/activities/MainActivity.java
+++ app/src/main/java/com/example/android/popularmovies/activities/MainActivity.java
@@ -1,39 +1,69 @@
 package com.example.android.popularmovies.activities;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Toast;
 
 import com.example.android.popularmovies.R;
+import com.example.android.popularmovies.databinding.ActivityMainBinding;
+import com.example.android.popularmovies.databinding.ListItemBinding;
 import com.github.clans.fab.FloatingActionMenu;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
 	
-	SwipeRefreshLayout swipeRefreshLayout;
+	private ActivityMainBinding binding;
+	
+	private SwipeRefreshLayout swipeRefreshLayout;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		// TODO Replace with DataBinding
-		setContentView(R.layout.activity_main);
+		binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 		
-		
-		Toolbar toolbar = findViewById(R.id.toolbar_main);
+		Toolbar toolbar = binding.toolbarMain;
 		setSupportActionBar(toolbar);
 		getSupportActionBar().setTitle(R.string.fab_label_popular);
 		
 		
-		swipeRefreshLayout = findViewById(R.id.swipe_refresh);
+		swipeRefreshLayout = binding.swipeRefresh;
 		swipeRefreshLayout.setOnRefreshListener(this);
-		
-		
-		FloatingActionMenu fam = findViewById(R.id.fab_base);
+
+
+		FloatingActionMenu fam = binding.fabBase;
 		fam.setIconAnimated(false);
 		fam.setClosedOnTouchOutside(true);
+		
+		
+		RecyclerView recyclerView = binding.recyclerView;
+		recyclerView.setLayoutManager(new GridLayoutManager(this, 2, LinearLayoutManager.VERTICAL, false));
+		recyclerView.setHasFixedSize(true);
+		recyclerView.setAdapter(new MovieAdapter(getTempTitles()));
+	}
+	
+	/**
+	 * FAKE DATA FOR RECYCLERVIEW
+	 */
+	private List<String> getTempTitles() {
+		List<String> temp = new ArrayList<>();
+		for (int x = 0; x < 200; x++) {
+			temp.add(String.valueOf(x));
+		}
+		return temp;
 	}
 	
 	
@@ -58,5 +88,54 @@ public class MainActivity extends AppCompatActivity implements SwipeRefreshLayou
 	public void onRefresh() {
 		Toast.makeText(this, "Placeholder for SwipeRefresh", Toast.LENGTH_SHORT).show();
 		swipeRefreshLayout.setRefreshing(false);
+		// showProgressBar
+	}
+	
+	
+	private class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHolder> {
+		
+		private List<String> tempTitles;
+		
+		MovieAdapter(List<String> tempTitles) {
+			this.tempTitles = tempTitles;
+		}
+		
+		@NonNull
+		@Override
+		public MovieViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+			return new MovieViewHolder(
+					ListItemBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false)
+			);
+		}
+		
+		@Override
+		public void onBindViewHolder(@NonNull MovieViewHolder holder, int position) {
+			holder.bind();
+		}
+		
+		@Override
+		public int getItemCount() {
+			return tempTitles.size();
+		}
+		
+		
+		class MovieViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+			
+			private ListItemBinding binding;
+			
+			MovieViewHolder(ListItemBinding binding) {
+				super(binding.getRoot());
+				this.binding = binding;
+			}
+			
+			private void bind() {
+				binding.movieTitle.setText(tempTitles.get(getAdapterPosition()));
+			}
+			
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(MainActivity.this, "ViewHolder clicked placeholder", Toast.LENGTH_SHORT).show();
+			}
+		}
 	}
 }
