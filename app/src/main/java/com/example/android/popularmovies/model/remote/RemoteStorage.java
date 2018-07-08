826565d8040cd8ef2fd18c62ee1564b906ab194d
DavidPrecopia <35779726+DavidPrecopia@users.noreply.github.com>
2018-07-07 20:16:45 -0400

    Combined RemoteStorage and Client.

--- app/src/main/java/com/example/android/popularmovies/model/remote/RemoteStorage.java
+++ app/src/main/java/com/example/android/popularmovies/model/remote/RemoteStorage.java
@@ -1,18 +1,20 @@
 package com.example.android.popularmovies.model.remote;
 
-import android.util.Log;
-
 import com.example.android.popularmovies.model.contracts_back.IRemoteStorage;
 import com.example.android.popularmovies.model.datamodel.Movie;
+import com.example.android.popularmovies.model.datamodel.MovieDbResponse;
 import com.example.android.popularmovies.model.datamodel.MovieDetails;
 
 import java.util.List;
 
 import io.reactivex.Single;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public final class RemoteStorage implements IRemoteStorage {
 	
-	private final Client client;
+	private final MovieDbService movieDbService;
 	
 	private static RemoteStorage remoteStorage;
 	
@@ -23,23 +25,47 @@ public final class RemoteStorage implements IRemoteStorage {
 		return remoteStorage;
 	}
 	
+	
 	private RemoteStorage() {
-		client = Client.getInstance();
+		movieDbService = new Retrofit.Builder()
+				.baseUrl(UrlManager.BASE_URL)
+//				.client(okHttpLogging())
+				.addConverterFactory(GsonConverterFactory.create())
+				.addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+				.build()
+				.create(MovieDbService.class);
+		
 	}
 	
+//	private OkHttpClient okHttpLogging() {
+//		OkHttpClient.Builder builder = new OkHttpClient.Builder();
+//		if (BuildConfig.DEBUG) {
+//			HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+//			logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+//			builder.addInterceptor(logging);
+//		}
+//		return builder.build();
+//	}
 	
+	
+	@Override
 	public Single<List<Movie>> getPopularMovies() {
-		Log.d("LOG_TAG", "getPopularMovies");
-		return client.getMovies(UrlManager.QUERY_POPULAR);
+		return getMovies(UrlManager.QUERY_POPULAR);
 	}
 	
+	@Override
 	public Single<List<Movie>> getHighestRatedMovies() {
-		return client.getMovies(UrlManager.QUERY_HIGHEST_RATED);
+		return getMovies(UrlManager.QUERY_HIGHEST_RATED);
+	}
+	
+	private Single<List<Movie>> getMovies(String sortBy) {
+		return movieDbService.sortedMovies(sortBy)
+				.map(MovieDbResponse::getMoviesList);
 	}
 	
 	
 	@Override
 	public Single<MovieDetails> getSingleMovie(int movieId) {
-		return client.getSingleMovie(movieId);
+		return movieDbService.singleMovie(movieId);
 	}
 }
